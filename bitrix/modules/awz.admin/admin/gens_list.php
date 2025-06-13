3a0bff34657b33aed6069999b7c96764c78ede00
Andrew Zahalski <andrew@zahalski.dev>
2025-06-14 00:17:19 +0300

    - рефакторинг;

--- bitrix/modules/awz.admin/admin/gens_list.php
+++ bitrix/modules/awz.admin/admin/gens_list.php
@@ -6,18 +6,23 @@ use Awz\Admin\Access\AccessController;
 use Awz\Admin\Access\Custom\ActionDictionary;
 
 global $APPLICATION;
-$dirs = explode('/',dirname(__DIR__ . '../'));
+$dirs = explode(DIRECTORY_SEPARATOR, dirname(__DIR__, 1));
 $module_id = array_pop($dirs);
 unset($dirs);
 Loc::loadMessages(__FILE__);
 
-if(!Loader::includeModule('awz.admin')) return;
 if(!Loader::includeModule($module_id)) return;
 
 if(!AccessController::can(0, ActionDictionary::ACTION_GENS_PAGE))
     $APPLICATION->AuthForm(Loc::getMessage("ACCESS_DENIED"));
 global $POST_RIGHT;
 $POST_RIGHT = 'W';
+
+if(file_exists('check_awz_admin.php')){
+    require_once('check_awz_admin.php');
+}elseif(!Loader::includeModule('awz.admin')){
+    return;
+}
 
 /* "Awz\Admin\AdminPages\GensList" replace generator */
 use Awz\Admin\AdminPages\GensList as PageList;
--- bitrix/modules/awz.admin/admin/gens_right.php
+++ bitrix/modules/awz.admin/admin/gens_right.php
@@ -6,16 +6,21 @@ use Awz\Admin\Access\AccessController;
 use Awz\Admin\Access\Custom\ActionDictionary;
 
 global $APPLICATION;
-$dirs = explode('/',dirname(__DIR__ . '../'));
+$dirs = explode(DIRECTORY_SEPARATOR, dirname(__DIR__, 1));
 $module_id = array_pop($dirs);
 unset($dirs);
 Loc::loadMessages(__FILE__);
 
-if(!Loader::includeModule('awz.admin')) return;
 if(!Loader::includeModule($module_id)) return;
 
 if(!AccessController::can(0, ActionDictionary::ACTION_GENS_RIGHT))
     $APPLICATION->AuthForm(Loc::getMessage("ACCESS_DENIED"));
+
+if(file_exists('check_awz_admin.php')){
+    require_once('check_awz_admin.php');
+}elseif(!Loader::includeModule('awz.admin')){
+    return;
+}
 
 /* "Awz\Admin\AdminPages\PageItemEdit" replace generator */
 use Awz\Admin\AdminPages\GensRight as PageItemEdit;
