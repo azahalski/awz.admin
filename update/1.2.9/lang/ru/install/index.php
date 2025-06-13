22ca22b0adb4b6a39097fb54ffb7fd31f5759359
Andrew Zahalski <andrew@zahalski.dev>
2025-06-13 20:19:47 +0300

    gulp checkpoint

---
+++ update/1.2.9/lang/ru/install/index.php
@@ -0,0 +1,9 @@
+<?php
+$MESS['AWZ_ADMIN_MODULE_NAME'] = 'AWZ: Конструктор списков';
+$MESS['AWZ_ADMIN_MODULE_DESCRIPTION'] = 'Api для быстрого создания административных страниц';
+$MESS["AWZ_PARTNER_NAME"] = "Andrew Zahalski";
+$MESS["AWZ_PARTNER_URI"] = "https://zahalski.dev";
+$MESS["AWZ_ADMIN_INSTALL_TITLE"] = "Удаление модуля AWZ: Конструктор списков";
+$MESS['MOD_UNINST_SAVE_OPTS'] = "Вы можете сохранить основные настройки модуля";
+$MESS['MOD_UNINST_SAVE_EVENTS_DESC'] = "Вы можете сохранить почтовые шаблоны";
+$MESS['MOD_UNINST_SAVE_OPTS_LABEL'] = "Сохранить настройки";
