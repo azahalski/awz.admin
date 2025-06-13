39283c88bb54ac7650ffad10d2d1ee26563f66b9
Andrew Zahalski <andrew@zahalski.dev>
2025-06-14 00:14:44 +0300

    - изменение шаблонов административных страниц генератора;

--- bitrix/modules/awz.admin/install/version.php
+++ bitrix/modules/awz.admin/install/version.php
@@ -1,6 +1,6 @@
 <?
 $arModuleVersion = array(
-    "VERSION" => "1.2.11",
-    "VERSION_DATE" => "2025-06-13 20:11:00"
+    "VERSION" => "1.2.12",
+    "VERSION_DATE" => "2025-06-14 00:11:00"
 );
 ?>
